9c9a0993e54ead1d650d9051c2ab345a0035c73c
Ricardostf <64339671+Ricardostf@users.noreply.github.com>
2022-12-03 05:59:36 -0300

    Create B.java

---
+++ B.java
@@ -0,0 +1,28 @@
+public class B {
+  private int B1;
+  private double B2;
+  
+  public B(){
+    B1 = 1;
+    B2 = 0.333;
+  }
+  
+  public int getB1(){
+    return B1;
+  }
+  public double getB2(){
+    return B2;
+  }
+  public void setB1(int x){
+    B1 = x;
+  }
+  public void setB2(double x){
+    B2 = x;
+  }
+  public void MB1(){
+    system.out.println("MB1");
+  }
+  public void MB2(){
+    system.out.println("MB2");
+  }
+}
