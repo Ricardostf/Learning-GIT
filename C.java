e3672c1597ec7abaafa8f1aa5678c5021f96fcdc
Ricardo Augusto Santos <ricardosantos.ook@gmail.com>
2022-12-03 06:19:13 -0300

    Criacao da classe C

---
+++ C.java
@@ -0,0 +1,28 @@
+public class C {
+  private int C1;
+  private double C2;
+  
+  public C(){
+    C1 = 1;
+    C2 = 0.333;
+  }
+  
+  public int getC1(){
+    return C1;
+  }
+  public double getC2(){
+    return C2;
+  }
+  public void setC1(int x){
+    C1 = x;
+  }
+  public void setC2(double x){
+    C2 = x;
+  }
+  public void MC1(){
+    System.out.println("MC1");
+  }
+  public void MC2(){
+    System.out.println("MC2");
+  }
+}
