c9fedff73366f6a4a8ef60aab901c7e4e181c4cd
Ricardo Augusto Santos <ricardosantos.ook@gmail.com>
2022-12-04 15:02:07 -0300

    add Classe D

---
+++ D.java
@@ -0,0 +1,29 @@
+public class D {
+    private int D1;
+    private double D2;
+    
+    public D(){
+      D1 = 1;
+      D2 = 0.333;
+    }
+    
+    public int getD1(){
+      return D1;
+    }
+    public double getD2(){
+      return D2;
+    }
+    public void setD1(int x){
+      D1 = x;
+    }
+    public void setD2(double x){
+      D2 = x;
+    }
+    public void MD1(){
+      System.out.println("MD1");
+    }
+    public void MD2(){
+      System.out.println("MD2");
+    }
+  }
+  
